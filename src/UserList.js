9f2228b86b1015753d3f14bcf42a15f59f69932e
jaisleek <babajide.babalola@yahoo.com>
2024-09-15 15:12:13 +0100

    Initial commit

---
+++ src/UserList.js
@@ -0,0 +1,32 @@
+// src/UserList.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UserList = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setListOfUsers(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the user data!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>List of Users</h1>
+      <ul>
+        {listOfUsers.map(user => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
